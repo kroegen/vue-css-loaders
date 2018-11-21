003024e33fe283d3bb1bd4a845a05e321167409d
Yevhenii Krotenko <kroegen4@gmail.com>
2018-11-21 21:49:12 +0200

    fix

--- src/utils/baseUtils.js
+++ src/utils/baseUtils.js
@@ -2,10 +2,10 @@ export function handleAddRule(selector, rule) {
   const sheet = document.styleSheets[0];
 
   try {
-    sheet.insertRule(`${selector} { ${rule} }`);
+    sheet.insertRule(`${selector} { ${rule} }`, 0);
   } catch(msie) {
     try {
-      sheet.addRule(selector, rule);
+      sheet.addRule(selector, rule, 0);
     } catch(err) {
       console.error(err); // eslint-disable-line
       throw err;
